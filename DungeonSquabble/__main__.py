407a7d871fda410d6322a502fc978f8729acedce
Martin Balmaceda <martin.balmaceda@gmail.com>
2014-10-05 10:44:55 +0200

    directors and scenes

--- DungeonSquabble/__main__.py
+++ DungeonSquabble/__main__.py
@@ -1,6 +1,17 @@
+from core import control
+from core import director
+
+from consts import DISPLAY_SIZE, DISPLAY_RESOLUTION
+from scenes import GameScene
 
 def main():
     """ your app starts here
     """
 
-    print 'running'
+    gs = GameScene()
+
+    control.run(gs, DISPLAY_SIZE, DISPLAY_RESOLUTION)
+
+    #controller = Controller(DISPLAY_SIZE, DISPLAY_RESOLUTION)
+    #controller.run(None)
+
