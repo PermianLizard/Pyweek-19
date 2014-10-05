90f85b05da05c1a371d90216eeea71c29239c81f
Martin Balmaceda <martin.balmaceda@gmail.com>
2014-10-05 08:57:20 +0200

    init

---
+++ DungeonSquabble/__main__.py
@@ -0,0 +1,6 @@
+
+def main():
+    """ your app starts here
+    """
+
+    print 'running'
---
+++ run_game.py
@@ -0,0 +1,3 @@
+import DungeonSquabble.__main__
+if __name__ == "__main__":
+    DungeonSquabble.__main__.main()
